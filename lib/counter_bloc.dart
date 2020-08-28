aba435568cd33c3d214a7d797c43452069045922
javeedishaq <ishaqjaveed1@gmail.com>
2020-08-28 20:07:22 +0500

    app

---
+++ lib/counter_bloc.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+
+enum CounterAction { Increment, Decrement, Reset }
+
+class CounterBloc {
+  int counter;
+
+  final _stateStreamController = StreamController<int>();
+
+  StreamSink<int> get counterSink => _stateStreamController.sink;
+  Stream<int> get counterStream => _stateStreamController.stream;
+
+  final _eventStreamController = StreamController<CounterAction>();
+
+  StreamSink<CounterAction> get eventSink => _eventStreamController.sink;
+  Stream<CounterAction> get eventStream => _eventStreamController.stream;
+
+  CounterBloc() {
+    counter = 0;
+    eventStream.listen((event) {
+      if (event == CounterAction.Increment) {
+        counter++;
+      } else if (event == CounterAction.Decrement) {
+        counter--;
+      } else if (event == CounterAction.Reset) {
+        counter = 0;
+      }
+    });
+  }
+}
